7522a4d3c406bacf71cc67e0fed8639349304bb9
Pumpkin <klemon108@163.com>
2020-06-16 19:43:38 +0800

    g7

---
+++ epdimic/Group08/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Group08Config(AppConfig):
+    name = 'Group08'
---
+++ epdimic/epdimic/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
+
+from Group07 import dbrequest as g7
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('ESS/index', g7.rqst_process),
+    path('ESS/situation', g7.rqst_process),
+    path('ESS/situation$', g7.rqst_process),
+    path('ESS/situation/situationMoreInfo', g7.rqst_process),
+    path('ESS/situation/situationMoreInfo$', g7.rqst_process),
+    path('ESS/background/Cases', g7.rqst_process),
+    path('ESS/background/Cases$', g7.rqst_process),
+    url(r'^(?P<path>.*)$', serve, {'document_root': ''}),
+]
