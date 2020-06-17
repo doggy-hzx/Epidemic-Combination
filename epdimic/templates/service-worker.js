10ed3e74214c9031245573f6c2c495b6b9c587a4
thebigdoudou <15888690596@163.com>
2020-06-17 17:12:40 +0800

    add new

--- epdimic/templates/service-worker.js
+++ epdimic/templates/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d08c6f176a499fb815ac7e2a801d3479.js"
+  "/precache-manifest.d20bc999fdbbc44462b00c3b8996878e.js"
 );
 
 self.addEventListener('message', (event) => {
