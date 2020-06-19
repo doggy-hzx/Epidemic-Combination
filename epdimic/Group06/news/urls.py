4b1794b2eacaa1629df814f857a4a8343ea61148
bfy2000 <48590011+bfy2000@users.noreply.github.com>
2020-06-19 22:27:04 +0800

    Update urls.py

--- epdimic/Group06/news/urls.py
+++ epdimic/Group06/news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('NewsList/<slug:ori_type>', views.news_list),
-    path('NewsList/details/<int:newsid>/comment',views.view_news),
+    path('NewsList/details/<int:newsid>/comment',views.publish_comment),
     path('PublishNews/',views.publish_news),
+    path('NewsList/details/<int:newsid>/', views.view_news),
 ]
