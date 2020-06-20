6a66ba17e0162aab5e150c201dfbbcab82e20fc9
doggy-hzx <2229662841@qq.com>
2020-06-21 00:48:29 +0800

    修了管理一些bug

--- epdimic/Group06/news/urls.py
+++ epdimic/Group06/news/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('Newsdetails', views.view_news),
     path('NewsList/<slug:ori_type>', views.news_list),
-    path('Newsdetails/comment',views.publish_comment),
+    path('Newscomment',views.publish_comment),
     path('PublishNews',views.publish_news),
     path('Delnews',views.del_news),
     
